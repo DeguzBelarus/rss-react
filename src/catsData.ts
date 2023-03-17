c4de5651ed83ebc8cb963af0b6b8ff00dff17668
DeguzBelarus <deguz@mail.ru>
2023-03-17 21:00:19 +0300

    feat: add cats into the cats objects array

--- src/catsData.ts
+++ src/catsData.ts
@@ -9,4 +9,116 @@ export const catsData: Array<ICatObject> = [
     imageSrc:
       'https://chudo-prirody.com/uploads/posts/2021-08/1628723302_35-p-foto-mein-kunov-kotov-bolshikh-razmerov-46.jpg',
   },
+  {
+    id: 2,
+    breed: 'maine-coon',
+    homeCity: 'Vitebsk',
+    name: 'Monya',
+    imageSrc:
+      'https://prohvost.club/wp-content/uploads/2018/03/mramornyy-meyn-kun-s-ugryumym-vzglyadom-na-chernom-fone.jpg',
+  },
+  {
+    id: 3,
+    breed: 'abyssinian',
+    homeCity: 'Gomel',
+    name: 'Tutsi',
+    imageSrc:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Gustav_chocolate.jpg/800px-Gustav_chocolate.jpg',
+  },
+  {
+    id: 4,
+    breed: 'abyssinian',
+    homeCity: 'Grodno',
+    name: 'Markiz',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Valentino.jpg',
+  },
+  {
+    id: 5,
+    breed: 'american bobtail',
+    homeCity: 'Grodno',
+    name: 'Liza',
+    imageSrc:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/American_bobtail_2.jpg/800px-American_bobtail_2.jpg',
+  },
+  {
+    id: 6,
+    breed: 'american bobtail',
+    homeCity: 'Brest',
+    name: 'Murzik',
+    imageSrc: 'https://i.pinimg.com/originals/b8/81/34/b881345b242f370c103f9eeb2dd97554.jpg',
+  },
+  {
+    id: 7,
+    breed: 'balinese',
+    homeCity: 'Vilnius',
+    name: 'Nona',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Old-Style_Balinese_Cat.png',
+  },
+  {
+    id: 8,
+    breed: 'balinese',
+    homeCity: 'Tokio',
+    name: 'Minamoto',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/Ghislaine_6028.jpg',
+  },
+  {
+    id: 9,
+    breed: 'birman',
+    homeCity: 'Tokio',
+    name: 'Hasimoto',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Birman2.jpg',
+  },
+  {
+    id: 10,
+    breed: 'birman',
+    homeCity: 'Beijing',
+    name: 'Luiza',
+    imageSrc: 'https://upload.wikimedia.org/wikipedia/commons/0/06/Birmanstrofe.jpg',
+  },
+  {
+    id: 11,
+    breed: 'bengal',
+    homeCity: 'Prague',
+    name: 'Lola',
+    imageSrc:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Best_Stalk.jpg/1280px-Best_Stalk.jpg',
+  },
+  {
+    id: 12,
+    breed: 'bengal',
+    homeCity: 'Milan',
+    name: 'Zephyr',
+    imageSrc:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Catbengalinbedresting.jpg/1024px-Catbengalinbedresting.jpg',
+  },
+  {
+    id: 13,
+    breed: 'cornish rex',
+    homeCity: 'New York',
+    name: 'Star',
+    imageSrc:
+      'https://kartinkin.net/pics/uploads/posts/2022-09/thumbs/1662518670_15-kartinkin-net-p-poroda-koshek-kornish-reks-krasivo-15.jpg',
+  },
+  {
+    id: 14,
+    breed: 'cornish rex',
+    homeCity: 'Los Angeles',
+    name: 'Moona',
+    imageSrc: 'https://i.pinimg.com/originals/90/8d/12/908d123c4a38f8e8bad5eece1968654a.jpg',
+  },
+  {
+    id: 15,
+    breed: 'persian',
+    homeCity: 'Chicago',
+    name: 'Lollipop',
+    imageSrc: 'https://kot-pes.com/wp-content/uploads/2019/01/post_5c47684e9a452.jpg',
+  },
+  {
+    id: 16,
+    breed: 'persian',
+    homeCity: 'Istanbul',
+    name: 'CookieCat',
+    imageSrc:
+      'https://kartinkin.net/uploads/posts/2022-12/1670021350_kartinkin-net-p-pers-poroda-koshek-krasivo-48.jpg',
+  },
 ];
