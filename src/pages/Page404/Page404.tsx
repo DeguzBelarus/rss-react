4a4d6f85d114aa82f0072ecdff1304ba8e670b47
DeguzBelarus <deguz@mail.ru>
2023-03-18 12:35:30 +0300

    feat: complete page 404

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Page404.scss';
 
 export class Page404 extends Component {
   render() {
-    return <div className="page-404-wrapper">Page404 works!</div>;
+    return (
+      <div className="page-404-wrapper">
+        <h1>This page doesn&apos;t exist</h1>
+        <Link to={'/'} className="">
+          To the main page
+        </Link>
+      </div>
+    );
   }
 }
