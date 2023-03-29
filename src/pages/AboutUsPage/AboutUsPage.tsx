114943a8590974414b89d46274bfcb3d21c55b69
DeguzBelarus <deguz@mail.ru>
2023-03-29 19:05:31 +0300

    feat: change the Page404 and AboutUsPae components to functional with hooks

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 
 import { Header } from 'components/Header/Header';
 import './AboutUsPage.scss';
 
-export class AboutUsPage extends Component {
-  render() {
-    return (
-      <>
-        <Header origin="about-page" />
-        <div className="about-us-page-wrapper" data-testid="app-about-us-page">
-          <h1>About Us</h1>
-          <p>We are a cattery of various breeds of cats, where you can buy a cat that you like.</p>
-        </div>
-      </>
-    );
-  }
-}
+export const AboutUsPage: FC = () => {
+  return (
+    <>
+      <Header origin="about-page" />
+      <div className="about-us-page-wrapper" data-testid="app-about-us-page">
+        <h1>About Us</h1>
+        <p>We are a cattery of various breeds of cats, where you can buy a cat that you like.</p>
+      </div>
+    </>
+  );
+};
