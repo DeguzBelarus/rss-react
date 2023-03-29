7d65b94c9a952739e0fb70dc606e9065879ffc1c
DeguzBelarus <deguz@mail.ru>
2023-03-29 20:39:32 +0300

    feat: change the Header component to functional with hooks

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { HeaderOriginType } from 'types/types';
@@ -10,80 +10,65 @@ interface Props {
   filterKey?: string;
 }
 
-interface State {
-  isSearchMode: boolean;
-}
+export const Header: FC<Props> = ({ origin, filterKey, filterKeyUpdateData }) => {
+  const [isSearchMode, setIsSearchMode] = useState(false);
 
-export class Header extends Component<Props, State> {
-  state: State = {
-    isSearchMode: false,
+  const searchModeActivator = () => {
+    setIsSearchMode(true);
   };
 
-  searchModeActivator = () => {
-    this.setState({
-      isSearchMode: true,
-    });
+  const searchModeDeactivator = () => {
+    setIsSearchMode(false);
   };
-
-  searchModeDeactivator = () => {
-    this.setState({
-      isSearchMode: false,
-    });
-  };
-
-  render() {
-    const { origin, filterKey, filterKeyUpdateData } = this.props;
-    const { isSearchMode } = this.state;
-    return (
-      <header className={isSearchMode ? 'search-mode-header' : 'header'} data-testid="app-header">
-        {origin === 'main-page' && filterKeyUpdateData !== undefined && filterKey !== undefined ? (
-          <div className="filter-input-container">
-            <input
-              type="text"
-              className="filter-input"
-              placeholder="Enter a cat name..."
-              value={filterKey}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                filterKeyUpdateData?.(event.target.value || '')
-              }
-              onFocus={this.searchModeActivator}
-              onBlur={this.searchModeDeactivator}
-              data-testid="app-filter-input"
-            />
-          </div>
-        ) : null}
-        <div className="pages-links-container">
-          {origin === 'main-page' ? (
-            <>
-              <span className="current-page-span">Main</span>
-              <Link to={'/purchases'} data-testid="purchases-link">
-                Purchases
-              </Link>
-              <Link to={'/about'} data-testid="app-about-us-link">
-                About Us
-              </Link>
-            </>
-          ) : null}
-          {origin === 'about-page' ? (
-            <>
-              <Link to={'/'}>Main</Link>
-              <Link to={'/purchases'} data-testid="purchases-link">
-                Purchases
-              </Link>
-              <span className="current-page-span">About Us</span>
-            </>
-          ) : null}
-          {origin === 'purchases-page' ? (
-            <>
-              <Link to={'/'}>Main</Link>
-              <span className="current-page-span">Purchases</span>
-              <Link to={'/about'} data-testid="app-about-us-link">
-                About Us
-              </Link>
-            </>
-          ) : null}
+  return (
+    <header className={isSearchMode ? 'search-mode-header' : 'header'} data-testid="app-header">
+      {origin === 'main-page' && filterKeyUpdateData !== undefined && filterKey !== undefined ? (
+        <div className="filter-input-container">
+          <input
+            type="text"
+            className="filter-input"
+            placeholder="Enter a cat name..."
+            value={filterKey}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              filterKeyUpdateData?.(event.target.value || '')
+            }
+            onFocus={searchModeActivator}
+            onBlur={searchModeDeactivator}
+            data-testid="app-filter-input"
+          />
         </div>
-      </header>
-    );
-  }
-}
+      ) : null}
+      <div className="pages-links-container">
+        {origin === 'main-page' ? (
+          <>
+            <span className="current-page-span">Main</span>
+            <Link to={'/purchases'} data-testid="purchases-link">
+              Purchases
+            </Link>
+            <Link to={'/about'} data-testid="app-about-us-link">
+              About Us
+            </Link>
+          </>
+        ) : null}
+        {origin === 'about-page' ? (
+          <>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/purchases'} data-testid="purchases-link">
+              Purchases
+            </Link>
+            <span className="current-page-span">About Us</span>
+          </>
+        ) : null}
+        {origin === 'purchases-page' ? (
+          <>
+            <Link to={'/'}>Main</Link>
+            <span className="current-page-span">Purchases</span>
+            <Link to={'/about'} data-testid="app-about-us-link">
+              About Us
+            </Link>
+          </>
+        ) : null}
+      </div>
+    </header>
+  );
+};
