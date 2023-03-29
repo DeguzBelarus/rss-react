ae8cca8006140ecdc6b68319579889ee5801aac8
DeguzBelarus <deguz@mail.ru>
2023-03-29 19:37:00 +0300

    feat: change the CatItem component to functional with hooks

--- src/pages/MainPage/components/CatItem/CatItem.tsx
+++ src/pages/MainPage/components/CatItem/CatItem.tsx
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 
 import { ICatObject } from 'types/types';
 import './CatItem.scss';
 
-export class CatItem extends Component<ICatObject> {
-  render() {
-    const { breed, name, imageSrc, city, price } = this.props;
-    return (
-      <div className="cat-item-wrapper" data-testid="app-cat-item">
-        <p className="cat-name-paragraph">{name}</p>
-        <div className="cat-image-container">
-          <img src={imageSrc} alt="a cat" />
-        </div>
-        <div className="cat-info-container">
-          <span>{breed}</span>
-          <span className="price-span">{`${price}$`}</span>
-          <span>&#8962;{` ${city}`}</span>
-        </div>
+export const CatItem: FC<ICatObject> = ({ city, breed, imageSrc, name, price }) => {
+  return (
+    <div className="cat-item-wrapper" data-testid="app-cat-item">
+      <p className="cat-name-paragraph">{name}</p>
+      <div className="cat-image-container">
+        <img src={imageSrc} alt="a cat" />
       </div>
-    );
-  }
-}
+      <div className="cat-info-container">
+        <span>{breed}</span>
+        <span className="price-span">{`${price}$`}</span>
+        <span>&#8962;{` ${city}`}</span>
+      </div>
+    </div>
+  );
+};
