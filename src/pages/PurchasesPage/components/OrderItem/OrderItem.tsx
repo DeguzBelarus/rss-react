e15935c46cfae798e1d09d682c470bf365b45c50
DeguzBelarus <deguz@mail.ru>
2023-03-29 19:46:57 +0300

    feat: change the OrderItem component to functional with hooks

--- src/pages/PurchasesPage/components/OrderItem/OrderItem.tsx
+++ src/pages/PurchasesPage/components/OrderItem/OrderItem.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 
 import { IOrderObject } from 'types/types';
 import './OrderItem.scss';
@@ -7,31 +7,28 @@ interface Props extends IOrderObject {
   orderRemove: (id: number) => void;
 }
 
-export class OrderItem extends Component<Props> {
-  render() {
-    const { buyerInfo, catInfo, id, orderRemove } = this.props;
-    return (
-      <div className="order-item-wrapper" data-testid="app-order-item">
-        <span>{`#${id}`}</span>
-        <span>{catInfo.name}</span>
-        <img src={catInfo.imageSrc} alt="a cat preview" />
-        <div className="order-info-container">
-          <span>{`date: ${buyerInfo.purchaseDate}`}</span>
-          <span>{`owner: ${buyerInfo.name}`}</span>
-        </div>
-        {buyerInfo.profileImage ? (
-          <>
-            <span>avatar:</span>
-            <img src={buyerInfo.profileImage} alt="a cat preview" />
-          </>
-        ) : null}
-        <span className="cat-price-span">{`${catInfo.price}$`}</span>
-        {buyerInfo.isDeliveryNeeded === 'yes' ? <span>🚚</span> : <span>🏃</span>}
-        {buyerInfo.notificationConfirmation === 'yes' ? <span>✉️</span> : null}
-        <button type="button" className="order-cancel-button" onClick={() => orderRemove(id)}>
-          CANCEL
-        </button>
+export const OrderItem: FC<Props> = ({ buyerInfo, catInfo, id, orderRemove }) => {
+  return (
+    <div className="order-item-wrapper" data-testid="app-order-item">
+      <span>{`#${id}`}</span>
+      <span>{catInfo.name}</span>
+      <img src={catInfo.imageSrc} alt="a cat preview" />
+      <div className="order-info-container">
+        <span>{`date: ${buyerInfo.purchaseDate}`}</span>
+        <span>{`owner: ${buyerInfo.name}`}</span>
       </div>
-    );
-  }
-}
+      {buyerInfo.profileImage ? (
+        <>
+          <span>avatar:</span>
+          <img src={buyerInfo.profileImage} alt="a cat preview" />
+        </>
+      ) : null}
+      <span className="cat-price-span">{`${catInfo.price}$`}</span>
+      {buyerInfo.isDeliveryNeeded === 'yes' ? <span>🚚</span> : <span>🏃</span>}
+      {buyerInfo.notificationConfirmation === 'yes' ? <span>✉️</span> : null}
+      <button type="button" className="order-cancel-button" onClick={() => orderRemove(id)}>
+        CANCEL
+      </button>
+    </div>
+  );
+};
